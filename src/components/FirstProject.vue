b298f923f95665b15e436e7e5cd90a3e873d0340
programina <amina.belabbes@medionmail.com>
2021-04-02 23:34:34 +0200

    so much working - yeah

--- src/components/FirstProject.vue
+++ src/components/FirstProject.vue
@@ -1,62 +1,72 @@
 <template>
   <v-container>
+ 
     <v-row class="text-center">
+
       <v-col cols="12">
         <v-img
           :src="require('../assets/female-developer-vector.jpg')"
-          class="my-3"
+          class="mt-5"
           contain
           height="200"
         />
-
       </v-col>
 
-      <v-col class="mb-4">
+      <v-col >
         <div class="heading1">
-         First Project
+        First Project
         </div>
         <div class="heading2 mb-3">Subheading</div>
       </v-col>
-
-      <v-col
+       
+       <v-col
         class="mb-5"
         cols="12"
       >
+       <router-link to="/contact">
+         <v-btn
+              href="#"
+              target="_blank"
+              text
+              rounded
+              color="background"
+              class="button-gradient"
+            > 
+          <span class="mr-2">Get in touch</span>
+        
+        </v-btn>
+       </router-link>
+       
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
+      </v-col>
+
+
+      <v-col
+        class="mb-5 px-8"
+        cols="12"
+        justify="center"
+      >
+          <router-link
+            v-for="(card, i) in cards"
             :key="i"
-            :href="next.href"
-            class="subheading mx-3"
+            :to="card.href"
+            class="subheading mx-3 "
             target="_blank"
           >
             <v-card>
-              <v-card-title>TITLE CARD</v-card-title>
-              <v-card-text>Text</v-card-text>
+              <v-img
+                height="250"
+                :src="require('@/assets/' + card.imgSrc + '.jpg')"
+              ></v-img>
+              
+              <v-card-title>{{card.title}}</v-card-title>
+              <v-card-text><router-link :to="card.href">See more</router-link></v-card-text>
             </v-card>
-          </a>
-        </v-row>
+            <br/>
+          </router-link>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-       
-          <v-btn
-            href="#"
-            target="_blank"
-            text
-            rounded
-            color="background"
-            class="button-gradient"
-          > 
-        <span class="mr-2">Get in touch</span>
-       
-      </v-btn>
-
-      </v-col>
+     
 
     </v-row>
   </v-container>
@@ -69,8 +79,19 @@
     data: () => ({
       cards: [
         {
-          text: 'Explore components',
-          href: '/api-explorer',
+          title: 'Habit App',
+          href: '/habit-app',
+          imgSrc: '1'
+        },
+        {
+          title: 'Fitness App',
+          href: '/fitness-app',
+          imgSrc: '2'
+        },
+        {
+          title: 'Gamification App',
+          href: '/gamification-app',
+          imgSrc: '3'
         }
       ],
     }),
@@ -78,7 +99,6 @@
 </script>
 
 <style lang="scss" scoped>
-
   .button-gradient {
     background: transparent linear-gradient(351deg, #0093E9 0%, #75CBCA 40%, #80D0C7 49%, #80D0C7 100%) 0% 0% no-repeat padding-box;
     box-shadow: 0px 3px 6px #00000029;
